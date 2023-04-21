faa0f60c54c1dc03724028f54eb4a1e9da3a9966
Jeedom <Philippe Morry>
2023-04-21 08:49:01 +0200

    correction script bashrc installation + ajout commandes à ajouter en cas de non présence dans .bashrc

--- bash/install_bashrc.sh
+++ bash/install_bashrc.sh
@@ -1,18 +1,19 @@
 # installation du bashrc et du répertoire .bashrc.d
-echo "Installation de l'extension .bashrc dans $HOME"
+bashrcdir="$HOME/.bashrc.d/"
+export bashrcdir
+echo "Installation de l'extension .bashrc dans ~/$bashrcdir"
 # cat ./.bashrc >> ~/.bashrc
-
-if [ -d ~/.bashrc.d ];
-    then
-        # Source all .bashrc files 
-        for file in ./.bashrc.d/*.bashrc; 
-            do
-            cp -vu "$file" ~/.bashrc.d/
-            done
-    else
-        mkdir ~/.bashrc.d
-        for file in ./.bashrc.d/*.bashrc; 
-        do
-            cp -vu "$file" "~/.bashrc.d/" 
-        done
+if [ -d "$bashrcdir" ];
+then
+    echo "Repertoire $bashrcdir existe déjà, on passe à la copie des fichiers"
+else
+    mkdir "$bashrcdir"
 fi
+#Copie des fichiers locaux
+for file in ./.bashrc.d/*.bashrc; 
+do
+    cp -vu "$file" "$bashrcdir"
+done
+echo "Relancer un terminal pour activer les alias"
+echo "En cas de pb, il faut rajouter les lignes suivantes dans ~/.bashrc"
+cat ./bashrc_add.txt
